a39719a2439fcac82bf8070abe909b08d357a19a
sohamp321 <sohamp321@gmail.com>
2025-06-11 16:51:38 +0530

    Header, Footer and Note added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Header />
+      <Note />
       <Footer />
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Header() {
   const year = new Date().getFullYear();
-  return <footer>Copyright {year}</footer>;
+  return (
+    <footer>
+      <p>Copyright {year} </p>
+    </footer>
+  );
 }
 
 export default Header;

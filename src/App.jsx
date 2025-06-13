35dcf14e37c7087d0d43a9ca2f1922bf8650ac94
sohamp321 <sohamp321@gmail.com>
2025-06-13 16:27:52 +0530

    Added dynammic notes display

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
+import notes from "./notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
